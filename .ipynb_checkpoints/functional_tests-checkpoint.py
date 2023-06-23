25d1422768c4f93c22c38441035eec30cc72f0ad
LVKinyanjui <leslievictorius@gmail.com>
2023-06-23 14:10:56 +0300

    Front page HTML now generated from a template

--- .ipynb_checkpoints/functional_tests-checkpoint.py
+++ .ipynb_checkpoints/functional_tests-checkpoint.py
@@ -1,12 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 class NewVisitorTest(unittest.TestCase): 
 
      def setUp(self): 
-        self.browser = webdriver.Chrome('C:/Users/USER/.webdriver/chromedriver.exe')
+        
+        driver_path = 'C:/Users/USER/.webdriver/chromedriver.exe'
+        service = ChromeService(executable_path=driver_path)
+        self.browser = webdriver.Chrome(service=service)
      
      def tearDown(self): 
         self.browser.quit()
@@ -38,7 +43,7 @@ class NewVisitorTest(unittest.TestCase):
         table =     self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr') 
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
         )
         
         # There is still a text box inviting her to add another item. She
